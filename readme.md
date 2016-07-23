0d9721bb3d16571ca43f5faccb30215df34b2270
Kapil Verma <kapv89@gmail.com>
2016-07-23 12:22:26 +0530

    need to test it once

---
+++ readme.md
@@ -0,0 +1,11 @@
+# checkshape
+
+```js
+const Shape = require('checkshape');
+const s = new Shape({
+  user_id: (userId) => userExists(userId),
+  name: (name. {user_id}) => findUserByName(name).then((user) => user.id === user_id)
+})
+
+s.check(data).then(({result, errors}) => {})
+```
---
+++ src/Shape.js
@@ -0,0 +1,92 @@
+/**
+ * Promise based validation of input
+ *
+
+const Shape = require('checkshape');
+const s = new Shape({
+  user_id: (userId) => userExists(userId),
+  name: (name. {user_id}) => findUserByName(name).then((user) => user.id === user_id)
+})
+
+s.check(data).then(({result, errors}) => {})
+*/
+
+const {assign, isArray, toPlainObject} = require('lodash');
+
+const isUsableObject = require('isusableobject');
+
+class Shape {
+  constructor(validations=[]) {
+    this.validations = new Map();
+
+    this.addValidations(validations);
+  }
+
+  addValidations(validations=[]) {
+    if (isUsableObject(validations)) {
+      validations = toPlainObject(validations);
+      validations = Object.keys(validations).map((k) => ({key: k, validation: validations[k]}));
+    }
+
+    validations.forEach(({key, validation}) => {
+      this.validations.set(key, validation);
+    });
+
+    return this;
+  }
+
+  addValidation({key, validation}) {
+    this.validations.set(key, validation);
+    return this;
+  }
+
+  merge(validator) {
+    Array.from(validator.validation.keys()).forEach((k) => {
+      this.validations.set(k, validator.validations.get(k));
+    });
+
+    return this;
+  }
+
+  errors(input={}) {
+    const keys = Array.from(this.validations.keys());
+
+    return Promise.all(
+      keys.map((key) => {
+        const result = this.validations.get(key)(input[key], input, key);
+
+        if (result instanceof Promise) {
+          return result.then((result) => {
+            return {key, result};
+          });
+        } else if (result instanceof Shape) {
+          return result.errors(input[key]).then((result) => {
+            return {key, result};
+          });
+        } else {
+          return {key, result};
+        }
+      })
+    )
+    .then((errors) => errors.filter(({error}) => {
+      return isArray(error) ? error.length > 0 : error;
+    }))
+    .then((errors) => {
+      if (errors.length === 0) {
+        return null;
+      } else {
+        return errors.reduce((all, {key, result}) => {
+          return assign(all, {
+            [key]: isUsableObject(result) ? toPlainObject(result) : result
+          });
+        }, {});
+      }
+    });
+  }
+
+  check(input={}) {
+    return this.errors(input).then((errors) => ({result: !!errors, errors}));
+  }
+}
+
+module.exports = Shape;
