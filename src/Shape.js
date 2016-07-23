6e13f4c9a47d31dcc7ec7cdb74489d24b8ab7554
Kapil Verma <kapv89@gmail.com>
2016-07-23 12:24:09 +0530

    fixed readme

--- src/Shape.js
+++ src/Shape.js
@@ -5,7 +5,7 @@
 const Shape = require('checkshape');
 const s = new Shape({
   user_id: (userId) => userExists(userId),
-  name: (name. {user_id}) => findUserByName(name).then((user) => user.id === user_id)
+  name: (name, {user_id}) => findUserByName(name).then((user) => user.id === user_id)
 })
 
 s.check(data).then(({result, errors}) => {})
